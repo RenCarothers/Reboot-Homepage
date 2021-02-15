9eb6d0490b92970d5aa7dcb73434d7d973d1f685
Ren Carothers <rencarothers@gmail.com>
2021-02-15 15:38:28 -0800

    updates for latest deploy

--- src/components/Play.js
+++ src/components/Play.js
@@ -2,9 +2,9 @@ const Play = () => {
 
     return (
             <div>
-                <iframe frameborder="0" src="https://itch.io/embed-upload/3365063?color=333333" allowfullscreen="" width="820" height="688">
+                <iframe frameborder="0" src="https://itch.io/embed-upload/3365116?color=333333" allowfullscreen="true" width="820" height="688">
                     <a href="https://rabbitofthemoon.itch.io/reboot">Play Reboot on itch.io</a>
-                </iframe>                    
+                </iframe>                  
             </div>
    );
 }   
